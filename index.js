47d69bd17e69ac0b5bcbb1b1ecfcb13ee6f17f0c
Lucas Beserra <lucasbeserrasilva18@gmail.com>
2020-01-21 16:00:06 -0300

    tp

--- index.js
+++ index.js
@@ -3,17 +3,25 @@ var fields = document.querySelectorAll("#form-user-create [name]");
 
 var user = {};
 
-fields.forEach(function(field,index){
+
+
+document.getElementById("form-user-create").addEventListener("submit", function(event){
     
-    if(field.name == "gender"){
-        
-        if(field.checked) user[field.name] = field.value;
-        
-    }else{
-        
-        user[field.name] = field.value;
-    }
+    event.preventDefault();
+    
+    fields.forEach(function(field,index){
+
+        if(field.name == "gender"){
+
+            if(field.checked) user[field.name] = field.value;
+
+        }else{
+
+            user[field.name] = field.value;
+        }
+
+    });
+
+    console.log(user);
     
 });
-
-console.log(user);
