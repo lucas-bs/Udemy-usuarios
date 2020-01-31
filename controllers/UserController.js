c8d554386232316af7d0a87e9c22dbb6ac07fd2a
Lucas Beserra <lucasbeserrasilva18@gmail.com>
2020-01-31 16:03:45 -0300

    controller added

---
+++ controllers/UserController.js
@@ -0,0 +1,30 @@
+class UserController {
+    
+    constructor(formId){
+        
+        this.formEl = document.getElementById(formId);
+        
+    }
+    
+    getValues(){
+        
+        let user = {};
+        
+        this.formEl.elements.forEach(function(field,index){
+
+            if(field.name == "gender"){
+
+                if(field.checked) user[field.name] = field.value;
+
+            }else{
+
+                user[field.name] = field.value;
+            }
+
+        });
+
+        return new User(user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin);
+
+    }
+    
+}
--- index.js
+++ index.js
@@ -5,7 +5,20 @@ var user = {};
 
 function addLine(dataUser){
     
+    console.log(dataUser);
     
+    document.getElementById("table-users").innerHTML = `
+                <tr>
+                    <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                    <td>${dataUser.name}</td>
+                    <td>${dataUser.email}</td>
+                    <td>${dataUser.admin}</td>
+                    <td>${dataUser.birth}</td>
+                    <td>
+                      <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                      <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                    </td>
+                </tr>`;
     
 }
 
@@ -27,7 +40,9 @@ document.getElementById("form-user-create").addEventListener("submit", function(
         }
 
     });
+    
+    var objectUser = new User(user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin);
 
-    addLine(user);
+    addLine(objectUser);
     
 });
